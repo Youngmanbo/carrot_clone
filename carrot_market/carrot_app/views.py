a830f6a4003d7293c4259d049ce444ddff104cb0
Youngmanbo <yo900326@gmail.com>
2023-09-25 17:51:04 +0900

    feat 동네인증 기능 추가  모델 userprofile 추가 뷰 location set_region set_region)certification 추가  로그인기능 추가후 다시 수정필요

--- carrot_market/carrot_app/views.py
+++ carrot_market/carrot_app/views.py
@@ -1,19 +1,49 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import JsonResponse
+
+from .models import Post,UserProfile
 
 # Create your views here.
 
 def main(request):
     return render(request, "main.html")
 
+@login_required
 def location(request):
-    return render(request, 'carrot_app/location.html')
+    try:
+        user_profile = UserProfile.objects.get(user_id=request.user)
+        region = user_profile.region
+    except UserProfile.DoesNotExist:
+        region = None
 
+    return render(request, 'carrot_app/location.html', {'region': region})
+
+@login_required
 def set_region(request):
-    region = request.POST['region-setting']
-    return render(request, 'carrot_app/location.html', {'region':region})
+    if request.method == "POST":
+        region = request.POST.get('region-setting')
 
+        if region:
+            try:
+                user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+                user_profile.region = region
+                user_profile.save()
+
+                return redirect('carrot_app:location')
+            except Exception as e:
+                return JsonResponse({"status": "error", "message": str(e)})
+        else:
+            return JsonResponse({"status": "error", "message": "Region cannot be empty"})
+    else:
+        return JsonResponse({"status": "error", "message": "Method not allowed"}, status=405)
+
+# 지역인증 완료
+@login_required
 def set_region_certification(request):
-    if request.method == 'POST':
-        print(request.POST)
-    return redirect('dangun_app:location')
+    if request.method == "POST":
+        request.user.profile.region_certification = 'Y'
+        request.user.profile.save()
+        messages.success(request, "인증되었습니다")
+        return redirect('carrot_app:location')
