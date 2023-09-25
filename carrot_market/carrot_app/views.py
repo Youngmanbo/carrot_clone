b1d3386e862921cf41d119b71ee2622af7447391
Youngmanbo <yo900326@gmail.com>
2023-09-25 10:57:05 +0900

    feat : 동네인증 기능 구현   뷰 url 추가 및 수정

--- carrot_market/carrot_app/views.py
+++ carrot_market/carrot_app/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def main(request):
     return render(request, "main.html")
+
+def location(request):
+    return render(request, 'carrot_app/location.html')
+
+def set_region(request):
+    region = request.POST['region-setting']
+    return render(request, 'carrot_app/location.html', {'region':region})
+
+def set_region_certification(request):
+    if request.method == 'POST':
+        print(request.POST)
+    return redirect('dangun_app:location')
