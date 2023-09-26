e468ed64f32ac10f7fbd9cd7aba287fc1e273cc3
MinoKimm <jyys0531@gmail.com>
2023-09-26 14:46:18 +0900

    [Feat] add chat, logout in 'urls.py' for temporary

--- carrot_market/carrot_app/urls.py
+++ carrot_market/carrot_app/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
 from . import views
 
-app_name = 'dangun_app'
+# app_name = 'carrot_app' -> app_name 추가 시, templates를 읽어오지 못해서 주석처리
 
 urlpatterns = [
-    path('', views.main, name='main'),
-    path('location/', views.location, name='location'),
-    path('set_region/', views.set_region, name='set_region'),
-    path('set_region_certification/', views.set_region_certification, name='set_certification'),
+    # path('', views.main, name='main'),
+    # path('location/', views.location, name='location'),
+    # path('set_region/', views.set_region, name='set_region'),
+    # path('set_region_certification/', views.set_region_certification, name='set_certification'),
     path('', views.main, name='main'),
     path('write/', views.write, name='write'),
     path('trade/', views.trade, name='trade'),
     path('trade_post/<int:post_id>/', views.trade_post, name='trade_post'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('chat/', views.chat, name='chat'),
+    path('main/', views.logout, name='logout'),
 ]
