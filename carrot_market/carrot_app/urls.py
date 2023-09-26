a145e36b0a28241a30ad033764ad0d9f65d3e78f
Ethanall94 <Ethanall94@gmail.com>
2023-09-26 11:08:13 +0900

    [feat] urls.py 중복 수정

--- carrot_market/carrot_app/urls.py
+++ carrot_market/carrot_app/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'dangun_app'
-
 urlpatterns = [
     path('', views.main, name='main'),
     path('location/', views.location, name='location'),
     path('set_region/', views.set_region, name='set_region'),
     path('set_region_certification/', views.set_region_certification, name='set_certification'),
-    path('', views.main, name='main'),
     path('write/', views.write, name='write'),
     path('trade/', views.trade, name='trade'),
     path('trade_post/<int:post_id>/', views.trade_post, name='trade_post'),
